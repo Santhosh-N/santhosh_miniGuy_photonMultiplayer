bc1283c62d95ff083e0ad6dbd70552c69fed8d71
Santhosh-N <santhoshnagarajan321@gmail.com>
2023-03-16 04:09:51 +0530

    added project

--- Scripts/RandomMovement.cs
+++ Scripts/RandomMovement.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI; //important
+using UnityEngine.AI; 
 
-//if you use this code you are contractually obligated to like the YT video
-public class RandomMovement : MonoBehaviour //don't forget to change the script name if you haven't
+
+public class RandomMovement : MonoBehaviour 
 {
     public NavMeshAgent agent;
 
     public LayerMask whatIsGround;
 
-
-
-    //Patroling
     public Vector3 walkPoint;
     bool walkPointSet;
     public float walkPointRange;
@@ -37,13 +34,13 @@ public class RandomMovement : MonoBehaviour //don't forget to change the script 
 
         Vector3 distanceToWalkPoint = transform.position - walkPoint;
 
-        //Walkpoint reached
+
         if (distanceToWalkPoint.magnitude < 1f)
             walkPointSet = false;
     }
     private void SearchWalkPoint()
     {
-        //Calculate random point in range
+       
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
         float randomX = Random.Range(-walkPointRange, walkPointRange);
 
